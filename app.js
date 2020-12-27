cf4cbb79176ee8bb3f31101e524798fb5ead2502
Rajesh Bellapuknoda <rajesh.bellapukonda@gmail.com>
2020-12-27 13:40:28 +0000

    Replace encryption with Hashing for password using MD5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const express = require("express")
 const mongoose = require("mongoose")
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5")
 const PORT = process.env.PORT || 3000
 
 const app = express()
@@ -32,11 +32,7 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
-//Enable database encryption using Mongoose-encryption package
-//It requires Mongoose-encrypt package and uses mongoose plugin framework to encrypt and decrypt
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
-const User = mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema)
 
 app.get("/", function(req,res){
   res.render("home")
@@ -53,7 +49,7 @@ app.get("/register", function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
 
   newUser.save(function(err){
@@ -64,10 +60,13 @@ app.post("/register",function(req,res){
 })
 
 app.post("/login", function(req,res){
-  User.findOne({email: req.body.username}, function(err, foundUser){
+  const username = req.body.username
+  const password = md5(req.body.password)
+
+  User.findOne({email: username}, function(err, foundUser){
     if(!err){
       if(foundUser){
-        if (foundUser.password === req.body.password){
+        if (foundUser.password === password){
           res.render("secrets")
         }
       }
