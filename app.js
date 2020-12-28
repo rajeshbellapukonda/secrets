16fca02f4e2f7a65520faa373806be9db8563170
Rajesh Bellapuknoda <rajesh.bellapukonda@gmail.com>
2020-12-28 12:15:59 +0000

    Implement passport js for better security of account credentials

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const express = require("express")
 const mongoose = require("mongoose")
-const bcrypt = require("bcrypt")
-const saltRounds = 10
+const session = require("express-session")
+const passport = require("passport")
+const LocalStrategy = require('passport-local').Strategy
+const passportLocalMongoose = require("passport-local-mongoose")
+
 const PORT = process.env.PORT || 3000
 
 const app = express()
@@ -18,6 +21,17 @@ app.use(bodyParser.urlencoded({extended: true}))
 //configure public folder to download the files from. Files such as CSS, images etc.
 app.use(express.static("public"))
 
+//Configure express session to use in nodeJS
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+//To use passport in Express, it need to be initialised as below
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Connect to mongoose db
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
@@ -26,6 +40,7 @@ const dbHost=process.env.DB_HOST
 const dbConnectionURL="mongodb+srv://"+dbUser+":"+dbPassword+"@"+dbHost+"/userDB?retryWrites=true&w=majority"
 
 mongoose.connect(dbConnectionURL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useCreateIndex', true);
 
 //Create mongoose schema and mongoose model to connect with users collection in userDB
 const userSchema = new mongoose.Schema({
@@ -33,7 +48,17 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
+//Add Passport local mongoose as plugin to user schema to register and login users
+userSchema.plugin(passportLocalMongoose)
+
 const User = mongoose.model("User", userSchema)
+
+// Configures passport to use Local strategy i.e. use local username and password
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
   res.render("home")
@@ -47,44 +72,35 @@ app.get("/register", function(req,res){
   res.render("register")
 })
 
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets")
+  } else {
+    res.redirect("/")
+  }
+})
+
+app.get("/logout", function(req,res){
+  req.logout()
+  res.redirect("/")
+})
+
 app.post("/register",function(req,res){
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    // Store hash in your password DB.
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
+  User.register(new User({username: req.body.username}), req.body.password, function(err){
+    if (err){
+      console.log(err);
+      res.render("register")
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/secrets")
     })
-
-    newUser.save(function(err){
-      if (!err) {
-        res.render("secrets")
-      }
-    })
-  });
+  })
 
 
 })
 
-app.post("/login", function(req,res){
-  const username = req.body.username
-  const password = req.body.password
-
-  User.findOne({email: username}, function(err, foundUser){
-
-    if(!err){
-      if(foundUser){
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          // result == true
-          if (result === true){
-            res.render("secrets")
-          } else {
-            res.send("password do not match")
-          }
-        })
-      }
-    }
-  })
+app.post("/login", passport.authenticate("local", {failureRedirect: "/login"}), function(req, res){
+  res.redirect("/secrets")
 })
 
 //Start the server
